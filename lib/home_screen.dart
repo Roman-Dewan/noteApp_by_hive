81da02830f2420a5e83cc5c376527e1b86f6d2c0
Roman-Dewan <mdromandewan1888@gmail.com>
2025-12-26 22:43:51 +0600

    home screen set up processing

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -9,6 +9,9 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  final TextEditingController _titleTEController = TextEditingController();
+  final TextEditingController _descriptionTEController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,17 +20,73 @@ class _HomeScreenState extends State<HomeScreen> {
         centerTitle: true,
         backgroundColor: Colors.lightBlue,
       ),
-      body: Column(
-        children: [
+      body: Column(children: [
           
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          
+        onPressed: () async {
+          _showMyDialog();
         },
         child: Icon(Icons.add),
       ),
+    );
+  }
+
+  Future<void> _showMyDialog() async {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Add Data"),
+          content:  SingleChildScrollView(
+            child: Column(
+              children: [
+                // title
+                TextFormField(
+                  controller: _titleTEController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    hintText: "Title"
+                  ),
+                ), 
+                const SizedBox(height: 8),
+                // description
+                TextFormField(
+                  maxLines: 3,
+                  controller: _descriptionTEController,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    hintText: "Description"
+                  ),
+                ), 
+              ],
+            ),
+          ),
+          actions: [
+
+            // cancel button
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Cancel"),
+            ),
+            
+            // Add Button
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Add"),
+            ),
+          ],
+        );
+      },
     );
   }
 }
